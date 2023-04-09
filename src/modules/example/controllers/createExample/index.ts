3cd5bed14265d70225dda9500bdb37ee6b1cd5b7
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-04-09 14:43:52 -0300

    feat: install and use zod

--- src/modules/example/controllers/createExample/index.ts
+++ src/modules/example/controllers/createExample/index.ts
@@ -1,10 +1,15 @@
 import { CreateExampleUseCase } from '@/example/useCases/createExample';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { z } from 'zod';
+
+const createExampleScheme = z.object({
+  name: z.string().nonempty(),
+});
 
 class CreateExampleController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name } = request.body;
+    const { name } = createExampleScheme.parse(request.body);
 
     const createExampleUseCase = container.resolve(CreateExampleUseCase);
 
--- src/shared/infra/http/middlewares/parseErrors.ts
+++ src/shared/infra/http/middlewares/parseErrors.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { AppError } from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 export function parseErrors(
   error: Error,
@@ -8,6 +9,13 @@ export function parseErrors(
   response: Response,
   next: NextFunction
 ) {
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      status: 'validation error',
+      issues: error.issues,
+    });
+  }
+
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       message: error.message,
